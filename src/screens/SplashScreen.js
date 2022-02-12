d3c94cb7b2114f5d06d4190735fb11cfde84d5a2
Vladimir Salguero <vsalgueroerazo@gmail.com>
2022-02-12 00:53:18 -0600

    feat: logo splash screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {ActivityIndicator, View} from 'react-native';
+import {ActivityIndicator, View, Image} from 'react-native';
 
 const SplashScreen = () => {
   return (
     <View
       style={{flex: 1, justifyContent: 'center', backgroundColor: '#06bcee'}}>
       <ActivityIndicator size="large" color="#ffffff" />
+      <Image
+        style={styles.logo}
+        source={require('../assets/images/logo.png')}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 312,
+    height: 276,
+  },
+});
 
 export default SplashScreen;
